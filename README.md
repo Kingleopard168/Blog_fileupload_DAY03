906838ab452430ce79b1bb47df1a4b97cb1db9b7
Toungchai Sriangthong <114647008+Kingleopard168@users.noreply.github.com>
2022-12-05 13:12:42 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Blog_fileupload_DAY03
-How to create Template forms for comment and Css file
+# Project-blog-simple
+ Project-blog-simple
---
+++ my_site/blog/views.py
@@ -0,0 +1,54 @@
+from xml.dom.minidom import Identified
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView,DetailView
+
+from .models import Post
+from .forms import CommentForm
+# Create your views here.
+
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+#def starting_page(request):
+#    latest_posts = Post.objects.all().order_by("-date")[:3]
+#    return render(request, "blog/index.html",{
+#        "posts":latest_posts
+#    })
+
+class AllPostView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+#def posts(request):
+#    all_posts = Post.objects.all().order_by("-date")
+#    return render(request, "blog/all-posts.html" , {
+#        "all_posts": all_posts
+#    })
+    
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        context["comment_form"] = CommentForm()
+        return context
+
+#def post_datail(request, slug):
+#    Identified_post = get_object_or_404(Post, slug=slug)
+#    return render(request, "blog/post-detail.html", {
+#       "post":Identified_post,
+#        "post_tags": Identified_post.tags.all()
+#    })
+
